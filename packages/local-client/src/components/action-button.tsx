b0d884a65c0db8d2161348df2455195358ff22e1
OSfrog <tommy_8786@hotmail.com>
2023-04-15 16:55:19 +0200

    completed build

---
+++ packages/local-client/src/components/action-button.tsx
@@ -0,0 +1,16 @@
+interface ActionButtonProps {
+  onClick: () => void;
+  icon: string;
+}
+
+const ActionButton: React.FC<ActionButtonProps> = ({ onClick, icon }) => {
+  return (
+    <button className="button is-primary is-small" onClick={onClick}>
+      <span className="icon">
+        <i className={`fas ${icon}`} />
+      </span>
+    </button>
+  );
+};
+
+export default ActionButton;
